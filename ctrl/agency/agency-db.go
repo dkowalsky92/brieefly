740ef15a75633e329f1e313805c7b8f82ccff50d
Dominik Kowalski <d.kowalsky92@gmail.com>
2019-01-16 20:32:55 +0100

    added average opinion to get all agencies service

--- ctrl/agency/agency-db.go
+++ ctrl/agency/agency-db.go
@@ -3,6 +3,7 @@ package agency
 import (
 	"database/sql"
 
+	"github.com/brieefly/ctrl/agency/body"
 	"github.com/brieefly/db"
 	"github.com/brieefly/err"
 	"github.com/brieefly/model"
@@ -109,10 +110,10 @@ func DbGetForID(db *db.DB, id string) (*model.Agency, *err.Error) {
 }
 
 // DbGetAll - Get all agencies
-func DbGetAll(db *db.DB) ([]model.Agency, *err.Error) {
-	agencies := []model.Agency{}
+func DbGetAll(_db *db.DB) ([]body.Details, *err.Error) {
+	agencies := []body.Details{}
 
-	err := db.WithTransaction(func(tx *sql.Tx) *err.Error {
+	err := _db.WithTransaction(func(tx *sql.Tx) *err.Error {
 		rows, err := tx.Query(`SELECT a.agency_code,
 								a.nip_number, 
 								c.id_company, 
@@ -124,17 +125,19 @@ func DbGetAll(db *db.DB) ([]model.Agency, *err.Error) {
 								c.image_url, 
 								c.description, 
 								c.date_last_modified, 
-								c.date_created 
+								c.date_created
 								FROM Agency a 
 								INNER JOIN Company c ON a.id_company = c.id_company`)
 
 		if err != nil {
-			return db.HandleError(err)
+			return _db.HandleError(err)
 		}
 
 		for rows.Next() {
+			var d body.Details
 			var a model.Agency
 			var c model.Company
+
 			err = rows.Scan(&a.AgencyCode,
 				&a.NipNumber,
 				&c.ID,
@@ -148,12 +151,19 @@ func DbGetAll(db *db.DB) ([]model.Agency, *err.Error) {
 				&c.DateLastModified,
 				&c.DateCreated)
 			if err != nil {
-				return db.HandleError(err)
+				return _db.HandleError(err)
+			}
+
+			ao, err := DbGetAverageOpinionForAgencyID(_db, c.ID)
+			if err != nil {
+				return err
 			}
 
 			a.Company = c
+			d.Agency = &a
+			d.AverageOpinion = db.NullFloat64{Float64: *ao, Valid: true}
 
-			agencies = append(agencies, a)
+			agencies = append(agencies, d)
 		}
 
 		return nil
---
+++ ctrl/agency/agency-opinion-db.go
@@ -0,0 +1,31 @@
+package agency
+
+import (
+	"database/sql"
+
+	"github.com/brieefly/db"
+	"github.com/brieefly/err"
+)
+
+// DbGetAverageOpinionForAgencyID -
+func DbGetAverageOpinionForAgencyID(db *db.DB, id string) (*float64, *err.Error) {
+	var averageOpinion *float64
+
+	err := db.WithTransaction(func(tx *sql.Tx) *err.Error {
+		row := tx.QueryRow(`SELECT AVG(oo.grade) FROM Opinion oo
+									INNER JOIN Project p ON p.id_project = oo.id_project
+									INNER JOIN Offer o ON o.id_project = p.id_project
+									INNER JOIN Agency a ON a.id_company = o.id_company 
+									WHERE a.id_company = ?;`, id)
+		var ao float64
+		err := row.Scan(&ao)
+		if err != nil {
+			return db.HandleError(err)
+		}
+		averageOpinion = &ao
+
+		return nil
+	})
+
+	return averageOpinion, err
+}
